a698a15326f58e2105475f9c28a25b794e9b1baf
Leon <2446738735@qq.com>
2022-12-10 10:59:26 +0800

    no message

--- src/main/java/com/lyq/stream/StreamDemo1.java
+++ src/main/java/com/lyq/stream/StreamDemo1.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class StreamDemo1 {
     public static void main(String[] args) {
         //定义一个List集合
-        List<String> list1 = Arrays.asList("xx", "lisi", "zhagnsan", "wangwu");
+        List<String> list1 = Arrays.asList("xx", "lisi", "zhangnsan", "wangwu");
+        list1.stream().filter(name->name.startsWith("zhang")).forEach(System.out::println);
     }
 }
